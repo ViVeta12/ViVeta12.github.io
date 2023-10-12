eb5fcde0ab216bffccb2ae5d8f2ae1f08c5b903e
ViVeta12 <147594657+ViVeta12@users.noreply.github.com>
2023-10-12 20:13:55 +0900

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function mopen() {
+  document.getElementById("closedmenu").style.display = "none";
+  document.getElementById("menu").style.display = "block";
+}
+
+function mclose() {
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("closedmenu").style.display = "block";
+}
